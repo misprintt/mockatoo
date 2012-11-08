a6687fb63aa38bc18a65f62e5d65a3866df0e1b9
Dominic De Lorenzo <dom@misprintt.net>
2012-11-09 09:58:06 +1100

    Moved the macro function for calculating the default value Expr for a ComplexType into a separate class (Types)

--- src/mockatoo/macro/MockMaker.hx
+++ src/mockatoo/macro/MockMaker.hx
@@ -19,6 +19,8 @@ using tink.macro.tools.ExprTools;
 using tink.macro.tools.TypeTools;
 using tink.core.types.Outcome;
 
+using mockatoo.macro.Types;
+
 
 #if !haxe_211
 typedef TypeParamDecl = {
@@ -150,13 +152,13 @@ class MockMaker
 			switch(field.type)
 			{
 				case TInst(t, tparams):
-					arg.expr = getDefaultValueForType(field.type.toComplex(true));
+					arg.expr = field.type.toComplex(true).defaultValue();
 				case TType(t, tparams):
-					arg.expr = getDefaultValueForType(field.type.toComplex(true));
+					arg.expr = field.type.toComplex(true).defaultValue();
 				case TEnum(t, tparams):
-					arg.expr = getDefaultValueForType(field.type.toComplex(true));
+					arg.expr = field.type.toComplex(true).defaultValue();
 				case TFun(functionArgs, ret):
-					var e = getDefaultValueForType(field.type.toComplex(true));
+					var e = field.type.toComplex(true).defaultValue();
 					var fargs:Array<FunctionArg> = [];
 
 					for(a in functionArgs)
@@ -588,7 +590,7 @@ class MockMaker
 			for(arg in f.args)
 			{
 				trace(arg);
-				var argExpr = getDefaultValueForType(arg.type);
+				var argExpr = arg.type.defaultValue();
 				args.push(argExpr);
 			}
 			e = e.call(args);
@@ -651,7 +653,7 @@ class MockMaker
 
 			trace(field.name + ":" + Std.string(f.ret));
 		
-			var eDefaultReturnValue = getDefaultValueForType(f.ret); //default return type (usually 'null')
+			var eDefaultReturnValue = f.ret.defaultValue(); //default return type (usually 'null')
 
 			var eSuper =  ("super." + field.name).resolve().call(args);
 
@@ -701,6 +703,16 @@ class MockMaker
 
 		var fieldMeta = createMockFieldMeta(field, f);
 		field.meta.push(fieldMeta);
+
+
+		//validate optional args
+		for(arg in f.args)
+		{
+			//{ name => bar, type => TPath({ name => StdTypes, pack => [], params => [], sub => Bool }), opt => true, value => null }
+			if(!arg.opt) continue;
+
+			trace(arg.name + ":" + arg.type.toString());
+		}
 	}
 
 	function getFunctionArgIdents(f:Function)
@@ -785,66 +797,6 @@ class MockMaker
 			params:params
 
 		};
-	}
-
-	/**
-	Returns the default 'null' value for a type.
-
-	On static platforms (Flash, CPP, Java, C#), basic types have their own default values :
-
-	every Int is by default initialized to 0
-	every Float is by default initialized to NaN on Flash9+, and to 0.0 on CPP, Java and C#
-	every Bool is by default initialized to false
-	*/
-	function getDefaultValueForType(type:ComplexType):Expr
-	{
-		if(type == null)
-			return EConst(CIdent("null")).at();
-
-		var isStaticPlatform:Bool = false;
-		var isFlash:Bool = false;
-
-		var staticPlatforms = ["flash", "cpp", "java", "cs"];
-
-		for(platform in staticPlatforms)
-		{
-			if(Context.defined(platform))
-			{
-				isStaticPlatform = true;
-
-				if(platform == "flash") isFlash = true;
-				break;
-			}
-		}
-
-		if(isStaticPlatform)
-		{
-			switch(type)
-			{
-				case TPath(p):
-				{
-					if(p.pack.length != 0) return EConst(CIdent("null")).at();
-
-					if(p.name == "StdTypes") p.name = p.sub;
-
-					switch(p.name)
-					{
-						case "Bool":
-							return EConst(CIdent("false")).at();
-						case "Int":
-							return EConst(CInt("0")).at();
-						case "Float":
-							if(isFlash)
-								return "Math.NaN".resolve();
-							else
-								return EConst(CFloat("0.0")).at();
-						default: null;
-					}	
-				}
-				default: null;
-			}
-		}
-		return EConst(CIdent("null")).at();
 	}
 
 
